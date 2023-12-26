64cd42676b9ab7a295c01d3ba858d4ade4da8bc2
Carlo456 <gusvalenzuela77@gmail.com>
2023-12-26 12:13:28 -0600

    first commit

---
+++ app/Filament/Resources/PostResource.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PostResource\Pages;
+use App\Filament\Resources\PostResource\RelationManagers;
+use App\Models\Post;
+use Filament\Forms;
+use Filament\Forms\Set;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Str;
+
+class PostResource extends Resource
+{
+    protected static ?string $model = Post::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-document-text'; //heroicon-o-<insert icon name>
+
+    protected static ?string $navigationGroup = 'Content';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Section::make("Create a recipe")
+                ->columns([
+                    'sm' => 1,
+                    'xl' => 2,
+                    '2xl' => 2,
+                ])
+                ->description("This will contain the recipe")
+                ->schema([
+                    Forms\Components\TextInput::make('title')
+                    ->required()
+                    ->maxLength(2048)
+                    ->live()
+                    ->afterStateUpdated(fn (Set $set, ?string $state) => $set('slug', Str::slug($state))),
+                Forms\Components\TextInput::make('slug')
+                    ->required()
+                    ->maxLength(2048),
+                Forms\Components\RichEditor::make('body')
+                    ->required()
+                    ->columnSpanFull(),
+                Forms\Components\FileUpload::make('thumbnail')
+                    ->columnSpanFull(),
+                Forms\Components\Toggle::make('active')
+                    ->required(),
+                Forms\Components\DatePicker::make('published_at')
+                    ->required(),
+                Forms\Components\Select::make('categories')
+                    ->multiple()
+                    ->relationship('categories', 'title')
+                    ->required(),
+                ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('title')
+                    ->searchable(),
+                Tables\Columns\ImageColumn::make('thumbnail')
+                    ->searchable(),
+                Tables\Columns\IconColumn::make('active')
+                    ->boolean(),
+                Tables\Columns\TextColumn::make('published_at')
+                    ->dateTime()
+                    ->sortable(),
+                //Tables\Columns\TextColumn::make('user.name')
+                    //->numeric()
+                    //->sortable(),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make()
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPosts::route('/'),
+            'create' => Pages\CreatePost::route('/create'),
+            'view' => Pages\ViewPost::route('/{record}'),
+            'edit' => Pages\EditPost::route('/{record}/edit'),
+        ];
+    }    
+}
---
+++ resources/views/test-page.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <h2>This is the test page</h2>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam nihil, perferendis ut fuga sint autem, unde repellendus facilis amet nostrum natus qui ex perspiciatis cupiditate eum quaerat similique accusamus ad?</p>
+    <h3>{{$name}}</h3>
+</div>

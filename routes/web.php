d17a94e3dba6db738472a5807c539655ba8087a7
Carlo456 <gusvalenzuela77@gmail.com>
2023-12-27 13:45:37 -0600

    added inner post config to blog-cocina

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [PostController::class, 'index'])->name('home');
+Route::get('/{post:slug}', [PostController::class, 'show'])->name('view');
 
-
-Route::get('/test', function($name = "algo"){
-    return view('test-page', ['name' => $name]);
-});
 
 
 
